fd966312ca819874096e182c1a2b6f9452a437e9
triple1566 <ljhub1566@gmail.com>
2026-01-16 17:07:34 +0900

    added views and menus

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -6,6 +6,8 @@
     'depends':['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_property_menu.xml'
     ],
     'application':True
 }
