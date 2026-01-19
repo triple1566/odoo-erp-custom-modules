039aee80a037453247e88b49ec37c5a738d9d809
triple1566 <ljhub1566@gmail.com>
2026-01-19 19:56:20 +0900

    added compute field

--- addons/estate/models/realestate_clients.py
+++ addons/estate/models/realestate_clients.py
@@ -7,7 +7,22 @@ class realestate_clients(models.Model):
     name=fields.Char(required=True)
     age=fields.Integer()
     isvip=fields.Boolean()
-    profit=fields.Float()
+
+    profit=fields.Float(default=0)
+    loss=fields.Float(default=0)
+    networth=fields.Float(compute="_compute_networth")
+
     region=fields.Selection(string='Region',
         selection=[('canada','Canada'), ('america','America'), ('uk','UK'), ('austrailia','Austrailia')],
         help="Region the client resides in")
+    alive=fields.Boolean(default=True)
+
+    def neutralize_client(self):
+        self.alive=False
+        return
+
+    def _compute_networth(self):
+        for record in self:
+            record.networth = record.profit-record.loss
+            if record.networth<0:
+                record.networth=0
